fcccc8a2304c85e3843b092e36deb21176e15784
emy333 <emedoim333@gmail.com>
2023-10-30 10:10:23 -0300

    Arquivos Iniciais

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,10 @@
+
+function Filme(){
+    return(
+        <div>
+            <h1>Bem vindo a página Filmes</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+
+function Home(){
+    return(
+        <div>
+            <h1>Bem vindo a Página Inicial.</h1>
+        </div>
+    )
+}
+
+export default Home;
